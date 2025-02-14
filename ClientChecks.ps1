7bf86ff478109d1457874c5b5df4cb18a5db09da
QuickPots <102370841+quickpots@users.noreply.github.com>
2025-02-14 00:12:27 +0000

    Update ClientChecks.ps1

--- ClientChecks.ps1
+++ ClientChecks.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest "https://quickpots.fun/ScreenShare/tools/Windows/Other/ClientChecker.exe" -OutFile "ClientChecksTemp"
-run Clientcheckstemp.exe
+.\Clientcheckstemp
 Read-Host "press any key to continue............."
 Remove-Item Clientcheckstemp.exe
